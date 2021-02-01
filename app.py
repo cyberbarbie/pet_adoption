4a6ae15a49d10dcc571161a18cff858007464dbc
cyberbarbie <taeluralexis@gmail.com>
2021-02-01 16:41:04 -0500

    created routes and view functions for different pet types

--- app.py
+++ app.py
@@ -7,5 +7,31 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return "<h1>Adopt a Pet!</h1>"
+    return """<h1>Adopt a Pet!</h1>
+    <p>Browse through the links below to find your new furry friend:</p>
+    <ul>
+        <li><a href="/animals/dogs">Dogs</a></li>
+        <li><a href="/animals/cats">Cats</a></li>
+        <li><a href="/animals/rabbits">Rabbits</a></li>
+    </ul>
+    """
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+    html = f"<h1> List of {pet_type}</h1>"
+    html += "<ul>"
+
+    for pet in pets[pet_type]:
+        name = pet["name"]
+        html += f"<li>{name}</li>"
+
+    html += "</ul>"
+    return html
+
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+    pet = pets[pet_type][pet_id]
+    return pet
+
+
 
